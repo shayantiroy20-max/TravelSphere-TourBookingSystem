3cad1db43c4ffb820ec719a6886bba5661368957
N Biraja Isac <nbiraja.isacfcs@kiit.ac.in>
2026-05-23 01:43:51 +0530

    Disabled DB auto config

--- src/main/java/com/tourapp/TourbookingApplication.java
+++ src/main/java/com/tourapp/TourbookingApplication.java
@@ -1,7 +1,8 @@
 package com.tourapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TourbookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(TourbookingApplication.class, args);
