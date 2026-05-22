b159a126ba1a6ceb418d4bd9a339f0a04a0aedc6
N Biraja Isac <nbiraja.isacfcs@kiit.ac.in>
2026-05-23 01:48:49 +0530

    Disabled JPA and datasource

--- src/main/java/com/tourapp/TourbookingApplication.java
+++ src/main/java/com/tourapp/TourbookingApplication.java
@@ -1,10 +1,20 @@
 package com.tourapp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class TourbookingApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TourbookingApplication.class, args);
     }
+
 }
